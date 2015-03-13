17e285cea2473526ac30c8d8038d734b83c729cf
Christian Witting <witting.chr@gmail.com>
2015-03-13 12:41:41 +0100

    Day 1

--- Example1.py
+++ Example1.py
@@ -2,6 +2,7 @@
 import rospy
 import roslib; roslib.load_manifest('ardrone_python')
 from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist, Vector3
 
 if __name__ == '__main__':
     rospy.init_node('example_node', anonymous=True)
@@ -11,7 +12,13 @@ if __name__ == '__main__':
     pub_takeoff = rospy.Publisher('/ardrone/takeoff', Empty)
     pub_land = rospy.Publisher('/ardrone/land', Empty)
     pub_reset = rospy.Publisher('/ardrone/reset', Empty)
-    
+
+    rospy.sleep(1.0)
+    print("Reset")
+    pub_takeoff.publish(Empty())
+    pub_land.publish(Empty())
+    pub_reset.publish(Empty())
+
     print("ready!")
     rospy.sleep(1.0)
     
@@ -20,12 +27,12 @@ if __name__ == '__main__':
     rospy.sleep(6.0)
     
     print("Forward..")
-    pub_velocity.publish(Twist(vector3(0.05,0,0),Vector3(0,0,0)))
-    rospy.sleep(0.5)
+    pub_velocity.publish(Twist(Vector3(0.05,0,0),Vector3(0,0,0)))
+    rospy.sleep(0.1)
 
     print("Stop..")
-    pub_velocity.publish(Twist(vector3(0,0,0),Vector3(0,0,0)))
-    rospy.sleep(2)
+    pub_velocity.publish(Twist(Vector3(0,0,0),Vector3(0,0,0)))
+    rospy.sleep(1)
 
     print("land..")
     pub_land.publish(Empty())
