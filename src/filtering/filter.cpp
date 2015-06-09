579a762f9e246675b97a3a95fbd81cc264ed9d15
ChrisPB93 <prindsbilberg@Christopher-Prinds-Bilbergs-MacBook-Pro.local>
2015-06-09 10:22:49 +0200

    CMakeListst.txt added

--- src/filtering/filter.cpp
+++ src/filtering/filter.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 using namespace std;
 char key;
 
-IplImage* HSVfilter( IplImage* img) {
+IplImage* hsvFilter( IplImage* img) {
 
     //HSV image
     IplImage* imgHSV = cvCreateImage( cvGetSize(img), 8, 3);
@@ -19,26 +19,30 @@ IplImage* HSVfilter( IplImage* img) {
     IplImage* imgThresh = cvCreateImage( cvGetSize( img ), 8, 1 );
 
     cvInRangeS (imgHSV, cvScalar( 104, 178, 70), cvScalar(130, 240, 124), imgThresh );
+
+    //Clean up
+    cvReleaseImage( &imgHSV );  
+    return imgThresh;
 }
 
 int main()
 {
-    cvNamedWindow("Video", 1);    //Create window
-    cvNamedWindow("Filtered")
+        //Create window
+    
     CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);  //Capture using any camera connected to your system
     IplImage* frame = 0;
     while(1){ //Create infinte loop for live streaming
         
-        if(!(frame = cvQueryFrame(capture))) //Create image frames from capture
-            break;
+        frame = cvQueryFrame(capture);
 
-        IplImage* imgThresh = GetThresholdedImageHSV(frame);
+        IplImage* imgThresh = hsvFilter(frame);
             
-        }
         
         /// Show your results
-        namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
-        imshow( "Hough Circle Transform Demo", src );
+        namedWindow("Video", CV_WINDOW_AUTOSIZE);
+        imshow( "Video", cvarrToMat(frame) );
+        namedWindow("Filtered", CV_WINDOW_AUTOSIZE);
+        imshow( "Filtered", cvarrToMat(imgThresh));
         
         key = cvWaitKey(10);     //Capture Keyboard stroke
         if (char(key) == 27){
